a6b18b1fbf854542c8002802b41dde7126702cc2
Nakul Bhar <115773691+Orion729@users.noreply.github.com>
2023-09-06 02:06:55 +0530

    updates

---
+++ unit 1/pooja.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n, m;
+    std::cin >> n;
+
+    std::vector<int> arr1(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr1[i];
+    }
+
+    std::cin >> m;
+    std::vector<int> arr2(m);
+    for (int i = 0; i < m; ++i) {
+        std::cin >> arr2[i];
+    }
+
+    std::vector<int> merged(arr1.begin(), arr1.end());
+    merged.insert(merged.end(), arr2.begin(), arr2.end());
+
+    int maxScore = *std::max_element(merged.begin(), merged.end());
+    std::cout << maxScore << std::endl;
+
+    return 0;
+}
