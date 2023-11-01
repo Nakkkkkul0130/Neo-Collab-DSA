b148269e7833272b6a36b55af8522a932591486e
Nakul Bhar <115773691+Orion729@users.noreply.github.com>
2023-11-01 21:25:10 +0530

    resource scheduling software

---
+++ unit 5/in your company.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void maxHeapify(std::vector<int> &arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        std::swap(arr[i], arr[largest]);
+        maxHeapify(arr, n, largest);
+    }
+}
+
+void buildMaxHeap(std::vector<int> &arr) {
+    int n = arr.size();
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        maxHeapify(arr, n, i);
+    }
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+
+    buildMaxHeap(arr);
+
+    std::cout << "Max heap" << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "The maximum value is " << arr[0] << std::endl;
+
+    return 0;
+}
